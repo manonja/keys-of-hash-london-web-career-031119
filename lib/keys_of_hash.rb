eedd2d928917add7a2165637e93ade914efe3154
manon jacquin <manon.jacquin@gmail.com>
2019-03-05 09:54:13 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,5 @@
+# This method should take an undefined number of arguments, using the splat operator, and return an array with every key from the hash whose value matches the value(s) given as an argument. 
 class Hash
-  
   def keys_of(arguments)
     # code goes here
     arguments.each do |animal, country| 
